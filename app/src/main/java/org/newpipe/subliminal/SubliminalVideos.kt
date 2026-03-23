da51c79f222c8c01eac5d820e70fe48c48cbc661
nogitsunees <sakurachiyorin1@gmail.com>
2026-03-23 02:21:24 -0300

    Create SubliminalVideos.kt

---
+++ app/src/main/java/org/newpipe/subliminal/SubliminalVideos.kt
@@ -0,0 +1,7 @@
+package org.newpipe.subliminal
+
+val subliminalVideos = listOf(
+    Video("Exemplo Subliminal 1", "biokinesis para beleza", "https://youtu.be/LXthBnUAZY0"),
+    Video("Exemplo Subliminal 2", "subliminals para atração", "https://youtu.be/D13XFVwX4E4"),
+    Video("Exemplo Subliminal 3", "kinesis mental", "https://youtu.be/-DZxfZiGOH4")
+)
