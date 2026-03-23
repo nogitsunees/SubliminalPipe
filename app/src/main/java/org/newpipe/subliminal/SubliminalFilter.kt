e5e42cdd4f4f2cea3443f89058f87fc704cea776
nogitsunees <sakurachiyorin1@gmail.com>
2026-03-23 02:16:36 -0300

    Create SubliminalFilter.kt

---
+++ app/src/main/java/org/newpipe/subliminal/SubliminalFilter.kt
@@ -0,0 +1,13 @@
+package org.newpipe.subliminal
+
+data class Video(val title: String, val description: String, val url: String)
+
+fun filterSubliminal(videos: List<Video>): List<Video> {
+    val keywords = listOf("biokinesis", "kinesis", "luminal", "subliminals", "subliminal")
+    return videos.filter { video ->
+        keywords.any { keyword ->
+            video.title.contains(keyword, ignoreCase = true) ||
+            video.description.contains(keyword, ignoreCase = true)
+        }
+    }
+}
